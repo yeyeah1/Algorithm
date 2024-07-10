40beb8fb9667ea6112c1a91cd3354c3cbcec0103
yeyeah1 <2yeyeah1@gmail.com>
2024-07-10 11:06:21 +0900

    [Bronze IV] Title: 영수증, Time: 120 ms, Memory: 13036 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+
+		long X = sc.nextLong();
+		
+		int N = sc.nextInt();
+		
+		long sum = 0;
+		
+		for(int i = 0; i < N; i++) {
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			sum += a*b;
+			
+		}
+
+		if(X == sum) {
+			System.out.println("Yes");
+		}else
+			System.out.println("No");
+		
+	}//main
+
+}
