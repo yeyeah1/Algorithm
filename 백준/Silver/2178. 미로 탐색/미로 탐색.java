05d29007e0c255da3ca6521075d6f8c07b98f7c6
yeyeah1 <2yeyeah1@gmail.com>
2024-08-28 00:24:09 +0900

    [Silver I] Title: 미로 탐색, Time: 148 ms, Memory: 16856 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,86 +1,76 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static class Node {
-		int x;
-		int y;
+    public static class Node {
+        int x;
+        int y;
 
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
-	static int N, M, res;
-	static int[][] map, dist;
-	static boolean[][] visited;
-	// 상 하 좌 우
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
+    static boolean[][] visited;
+    static int N, M, res;
+    static int[][] maze, dist;
 
-	public static void main(String[] args) throws IOException {
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static void main(String[] args) throws IOException {
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		map = new int[N][M];
-		dist = new int[N][M];
-		visited = new boolean[N][M];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(str.substring(j, j + 1));
-			}
-		} // 미로 정보 입력
+        maze = new int[N][M];
+        dist = new int[N][M];
+        visited = new boolean[N][M];
 
-		// 0 : 벽, 1 : 길
-		dist[0][0] = 1;
-		visited[0][0] = true; // 시작지점 방문체크
-		bfs(0, 0);
-		System.out.println(dist[N - 1][M - 1]);
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String[] arr = st.nextToken().split("");
 
-	}// main
+            for (int j = 0; j < M; j++) {
+                maze[i][j] = Integer.parseInt(arr[j]);
+            }
+        } // 입력
 
-	static void bfs(int r, int c) {
+        dist[0][0] = 1;
+        visited[0][0] = true;
+        bfs(0, 0);
+        System.out.println(dist[N - 1][M - 1]);
 
-		Queue<Node> queue = new LinkedList<>();
 
-		queue.add(new Node(r, c)); // 큐에 현재 지점 넣어주기
+    } // main
 
-		// 큐가 비지 않는 동안 반복
-		while (!queue.isEmpty()) {
+    private static void bfs(int r, int c) {
+        Queue<Node> queue = new LinkedList<>();
 
-			Node now = queue.poll(); // 큐에서 하나 빼서
+        queue.add(new Node(r, c)); // add now location
 
-			// 델타탐색
-			for (int i = 0; i < 4; i++) {
+        while (!queue.isEmpty()) {
 
-				int nr = now.x + dr[i];
-				int nc = now.y + dc[i];
+            Node now = queue.poll();
 
-				// 범위 안에 들어오고 방문 안했고 다음 목적지가 길이라면
-				if (nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && map[nr][nc] == 1) {
-					queue.add(new Node(nr, nc)); // 큐에 넣어주기
-					visited[nr][nc] = true; // 방문체크
-					dist[nr][nc] = dist[now.x][now.y] + 1;
+            for (int i = 0; i < 4; i++) {
+                int nr = now.x + dr[i];
+                int nc = now.y + dc[i];
 
-				}
+                if (nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && maze[nr][nc] == 1) {
+                    queue.add(new Node(nr, nc));
+                    visited[nr][nc] = true;
+                    dist[nr][nc] = dist[now.x][now.y] + 1;
+                }
+            }
 
-			}
+        }
 
-		} // while
-
-	}// bfs
+    } // bfs
 
 }
